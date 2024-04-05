457f1008e6417f98e2aea7af08395f79a4765d15
RomainMIRAS <73794188+RomainMIRAS@users.noreply.github.com>
2024-04-05 10:27:50 +0200

    Setup Session

---
+++ ricm.web.GIVEN/impl/httpserver/itf/impl/Session.java
@@ -0,0 +1,25 @@
+package httpserver.itf.impl;
+
+import httpserver.itf.HttpSession;
+
+public class Session implements HttpSession {
+		
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getValue(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setValue(String key, Object value) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
